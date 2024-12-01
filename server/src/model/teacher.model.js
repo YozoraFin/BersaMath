b119d51d350a1483d6a03bfde18b61c22dd084e6
Atala <azleidszhea225@gmail.com>
2024-12-02 02:57:35 +0700

    feat(models): add core user models for students and teachers

---
+++ server/src/model/teacher.model.js
@@ -0,0 +1,77 @@
+import { DataTypes } from "sequelize";
+import db from "../config/db";
+
+export const Teacher = db.define(
+  "teacher",
+  {
+    teacher_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        len: [3, 100],
+      },
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+        len: [5, 100],
+      },
+    },
+    phone: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+      validate: {
+        len: [10, 15],
+        is: /^[0-9]+$/,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        len: [8, 255],
+      },
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        len: [0, 1000],
+      },
+    },
+    gender: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      validate: {
+        isIn: [["Pria", "Wanita"]],
+      },
+    },
+    profile_pict: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    refresh_token: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+Teacher.associate = (models) => {
+  Teacher.hasMany(models.Course, {
+    foreignKey: "teacher_id",
+    onDelete: "CASCADE",
+  });
+};
