1f7e1479dc99ecfd8d64ce7977b5830a341afabb
YozoraFin <arjunamarcel25@gmail.com>
2024-12-01 14:46:24 +0700

    Teacher init

--- admin/src/components/Sidebar.js
+++ admin/src/components/Sidebar.js
@@ -5,7 +5,9 @@ export default function Sidebar({ sidebar }) {
   return (
     <div className={sidebar ? 'sidebar sidebar-open' : 'sidebar'}>
       <div className="sidebar-profile">
-        <h3 className='brand-name'>BersaMath CMS</h3>
+        <div className="header w-100">
+          <h3 className='brand-name'>BersaMath CMS</h3>
+        </div>
         
       </div>
     </div>
