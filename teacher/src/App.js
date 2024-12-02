12cee133b087973fddf5d3050889f4f27502bfa8
YozoraFin <arjunamarcel25@gmail.com>
2024-12-02 22:21:23 +0700

    ngelengkapin sidebar + nambah animasi buat sidebar, nambain sedikit animasi di navbar,  ngerapihin shadow buat kontent, nambain beberapa file buat page selanjutnya

--- teacher/src/App.js
+++ teacher/src/App.js
@@ -3,17 +3,23 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
-import Siteconfig from './pages/Siteconfig';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Class from './pages/Class';
+import Course from './pages/Course';
+import Assignment from './pages/Assignment';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Routes>
-      <Route path='/aaa' element={<Login/>}/>
-      <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/bersamath' element={<Layout/>}>
         <Route index element={<Dashboard/>} />
-        <Route path='siteconfig' element={<Siteconfig/>} />
+        <Route path='kelas' element={<Class/>} />
+        <Route path='materi' element={<Course/>} />
+        <Route path='tugas' element={<Assignment/>} />
+        <Route path='profil' element={<Profile/>} />
       </Route>
     </Routes>
   );
--- teacher/src/pages/Siteconfig.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Siteconfig() {
-  return (
-    <div>Siteconfig</div>
-  )
-}
